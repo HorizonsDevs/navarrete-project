9639780ba6c3a8d84586e284a8ce0e1e4382ca8c
Steven Carrillo <62310686+httpsteven@users.noreply.github.com>
2025-02-06 21:03:15 -0700

    Language Updates to Map component, works well good stuff.

--- frontend-navarrete/src/lib/stores/languageStore.js
+++ frontend-navarrete/src/lib/stores/languageStore.js
@@ -1,5 +1,8 @@
 // src/stores/languageStore.js
 import { writable } from 'svelte/store';
 
-export const language = writable('en');
+// Get user's language from browser, fallback to English
+const userLang = navigator.language?.split('-')[0] || 'en';
+
+export const language = writable(userLang);
 export const translations = writable({});
